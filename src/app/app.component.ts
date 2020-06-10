66baa0795458c6e3e7dc4b39970a23dfe1e1f341
gielfull <gielfull@gmail.com>
2020-06-10 16:47:22 -0500

    Working upload image without endopint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,6 +48,16 @@ export class AppComponent  {
 		      console.log(editor.getContent());
           this.contentText = editor.getContent();
 		  });
+    },
+     /* without images_upload_url set, Upload tab won't show up*/
+    images_upload_url: '',
+
+    /* we override default upload handler to simulate successful upload*/
+    images_upload_handler: function (blobInfo, success, failure) {
+      setTimeout(function () {
+        /* no matter what you upload, we will turn it into TinyMCE logo :)*/
+        success('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
+      }, 2000);
     }
   };
 
